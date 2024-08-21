8f2ed448053ffbcf1873054f664e4b2088b8bf79
Maria Kasycheva <167572285+Kasycheva@users.noreply.github.com>
2024-08-21 05:16:48 +0200

    taks all

---
+++ js/task-4.js
@@ -0,0 +1,24 @@
+const form = document.querySelector('.login-form');
+
+form.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(event) {
+    event.preventDefault();
+
+    const email = event.currentTarget.elements.email.value.trim();
+    const password = event.currentTarget.elements.password.value.trim();
+
+    if (email === '' || password === '') {
+        alert('All form fields must be filled in');
+        return;
+    }
+
+    const formData = {
+        email,
+        password
+    };
+
+    console.log(formData);
+
+    form.reset();
+}
